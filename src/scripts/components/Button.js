338c90950bd9dd085f680c9f6df4485bff01cadd
Aleksandra202 <69905482+Aleksandra202@users.noreply.github.com>
2021-12-27 21:47:43 +0200

    Delete src/scripts directory

--- src/scripts/components/Button.js
+++
@@ -1,8 +0,0 @@
-const createButton = () => {
-  const button = document.createElement("button");
-  button.className.add("setting-button");
-
-  return button;
-};
-
-export default createButton;
--- src/scripts/components/Cell.js
+++
@@ -1,8 +0,0 @@
-const createCell = () => {
-  const cell = document.createElement("div");
-  cell.className.add("cell");
-
-  return cell;
-};
-
-export default createCell;
